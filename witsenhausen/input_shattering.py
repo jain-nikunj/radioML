6f923e30a31295936a9c8fbde22bb39ded30e06d
Nikunj Jain <nikunj.jain@berkeley.edu>
2018-04-19 16:26:03 -0700

    Added variable support

--- witsenhausen/input_shattering.py
+++ witsenhausen/input_shattering.py
@@ -8,8 +8,12 @@ PI = tf.constant(m.pi)
 np.random.seed(2018)
 tf.set_random_seed(2018)
 NUM_BINS = 6
+XMIN = -1
+XMAX = 1
+BIN_WIDTH = (XMAX - XMIN) / (NUM_BINS - 2)
 NUM_EPOCHS = 10000
 LR = 0.001
+BATCHSIZE = 1000
 
 def generate_samples(x):
   """
@@ -28,18 +32,14 @@ def generate_sample(x):
 
   # Implement decision rule here
 
-  output = np.zeros((NUM_BINS - 2, 1))
+  output = np.zeros((1, NUM_BINS - 2))
+  low = XMIN
 
-  if -1 < x < -0.5:
-    output[0, 0] = 1
-  elif x < 0:
-    output[1, 0] = 1
-  elif x < 0.5:
-    output[2, 0] = 1
-  elif x < 1:
-    output[3, 0] = 1
+  for i in range(NUM_BINS - 2):
+    if low + i * BIN_WIDTH < x <= low + (i + 1) * BIN_WIDTH:
+      output[0, i] = 1
 
-  return output.reshape((1, -1))
+  return output
 
 def sinc(x):
   atzero = tf.divide(tf.sin(tf.multiply(PI, x)), 1)
@@ -54,7 +54,8 @@ def create_network(num_bins):
 
   y = tf.add(tf.matmul(x, a), b)
 
-  input_mtx = tf.constant(shape=[1, (num_bins - 2)], value=1, dtype=tf.float32)
+  input_mtx = tf.Variable(tf.ones((1, (num_bins - 2)), tf.float32))
+  input_mtx = tf.constant(1, shape=[1, num_bins - 2], dtype=tf.float32)
   input_bias = tf.reshape(
     tf.constant(
       [-(i+1) + (num_bins + 1)/2 for i in range(num_bins - 2)],
@@ -76,7 +77,7 @@ def create_network(num_bins):
     acts, bins = sess.run([activations, bins], feed_dict = {x: [[0.5]]})
 
     for epoch in range(NUM_EPOCHS):
-      batch_x = np.random.uniform(low=-1, high=1, size=(100, 1))
+      batch_x = np.random.uniform(low=XMIN, high=XMAX, size=(BATCHSIZE, 1))
       batch_y = generate_samples(batch_x)
       batchcost, _, batchactivations = sess.run(
         [cost, train_op, activations], feed_dict = {x: batch_x, true_y: batch_y})
@@ -84,13 +85,18 @@ def create_network(num_bins):
       if epoch % 100 == 0:
         print("Cost: " + str(batchcost))
 
-    for batchx in np.linspace(-1, 1, 100):
+    for batchx in np.linspace(XMIN, XMAX, 100):
       batchx = np.matrix(batchx).reshape((1, 1))
       batchy = generate_samples((batchx))
       act = sess.run(
         [activations], feed_dict = {x:batchx, true_y:batchy})
       print("Actual: " + str(batchy) + " Predicted: " + str(act))
 
+    (n_a, n_b) = sess.run([a, b], feed_dict = {x: np.zeros((1, 1)),
+                                               true_y: np.zeros((1, NUM_BINS - 2))})
+
+    print("Learnt A: " + str(n_a))
+    print("Learnt B: " + str(n_b))
 
 
 create_network(NUM_BINS)
